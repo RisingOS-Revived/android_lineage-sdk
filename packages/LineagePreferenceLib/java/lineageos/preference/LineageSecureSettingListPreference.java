f7cf90cc88f40333dab4bafb8d55f382209f9e5e
Michael Bestas <mkbestas@lineageos.org>
2025-03-23 08:22:14 +0200

    sdk: Cleanup imports
    find . -name "*.java" | xargs java -jar \
    ~/bin/google-java-format-1.25.2-all-deps.jar \
    --aosp --fix-imports-only --replace
    
    Change-Id: If39579285c13cecdb3592d8ccf0605b8030b340e

--- packages/LineagePreferenceLib/java/lineageos/preference/LineageSecureSettingListPreference.java
+++ packages/LineagePreferenceLib/java/lineageos/preference/LineageSecureSettingListPreference.java
@@ -11,7 +11,6 @@ import android.util.AttributeSet;
 
 import lineageos.providers.LineageSettings;
 
-
 public class LineageSecureSettingListPreference extends SelfRemovingListPreference {
 
     private boolean mAutoSummary = false;
--- packages/LineagePreferenceLib/java/lineageos/preference/LineageSystemSettingListPreference.java
+++ packages/LineagePreferenceLib/java/lineageos/preference/LineageSystemSettingListPreference.java
@@ -11,7 +11,6 @@ import android.util.AttributeSet;
 
 import lineageos.providers.LineageSettings;
 
-
 public class LineageSystemSettingListPreference extends SelfRemovingListPreference {
 
     private boolean mAutoSummary = false;
