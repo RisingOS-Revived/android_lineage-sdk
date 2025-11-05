giste/beerdroid_lineage-sdk

Replace los versioning
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

sdk/src/java/lineageos/os/Build.java
@@ -28,10 +28,10 @@ public class Build {
     public static final String UNKNOWN = "unknown";
 
     /** A build ID utilized to distinguish lineageos versions */
-    public static final String LINEAGEOS_VERSION = getString("ro.lineage.version");
+    public static final String CRDROID_VERSION = getString("ro.crdroid.version");
 
     /** A build ID string meant for displaying to the user */
-    public static final String LINEAGEOS_DISPLAY_VERSION = getString("ro.lineage.display.version");
+    public static final String CRDROID_DISPLAY_VERSION = getString("ro.crdroid.display.version");
 
     private static final SparseArray<String> sdkMap;
     static

RisingOS-Revived/android_lineage-sdk

Change-Id: Ie10fea12fed88235c6df901596bd99e9a77c7ad4
Signed-off-by: AnierinB <anierin@evolution-x.org>

sdk/src/java/lineageos/os/Build.java
@@ -17,10 +17,10 @@ public class Build {
     public static final String UNKNOWN = "unknown";
 
     /** A build ID utilized to distinguish lineageos versions */
-    public static final String LINEAGEOS_VERSION = getString("ro.lineage.version");
+    public static final String EVOLUTION_VERSION = getString("ro.evolution.version");
 
     /** A build ID string meant for displaying to the user */
-    public static final String LINEAGEOS_DISPLAY_VERSION = getString("ro.lineage.display.version");
+    public static final String EVOLUTION_DISPLAY_VERSION = getString("ro.evolution.display.version");
 
     private static final SparseArray<String> sdkMap;
     static