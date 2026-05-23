cfc606d46717d2741cad1f1c49718fd6d6a1cf77
GHOST | ゴースト <90827302+Ghosuto@users.noreply.github.com>
2026-05-24 00:23:54 +0530

    lineagesettings: Bump shake gesture action max value to 18

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -1286,7 +1286,7 @@ public final class LineageSettings {
         
         /** @hide */
         public static final Validator KEY_SHAKE_GESTURE_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 17);
+                new InclusiveIntegerRangeValidator(0, 18);
 
         /**
          * Whether to wake the screen with the home key, the value is boolean.
