b177435696c24d27d1fe010ab9625fd29c64ca3d
Ghosuto <clash.raja10@gmail.com>
2026-07-15 13:15:22 +0000

    Add statusbar popup action [3/3]
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -374,7 +374,7 @@ public final class LineageSettings {
             new InclusiveIntegerRangeValidator(0, 86400);
 
     private static final Validator sHardwareKeyActionvalidator =
-            new InclusiveIntegerRangeValidator(0, 20);
+            new InclusiveIntegerRangeValidator(0, 21);
 
     private static final Validator sAlwaysTrueValidator = new Validator() {
         @Override
@@ -1099,6 +1099,7 @@ public final class LineageSettings {
          * 18 - Toggle Qs Panel
          * 19 - Ringer Modes
          * 20 - Ambient display
+         * 21 - System Popup
          */
         public static final String KEY_HOME_LONG_PRESS_ACTION = "key_home_long_press_action";
 
@@ -1287,7 +1288,7 @@ public final class LineageSettings {
         
         /** @hide */
         public static final Validator KEY_SHAKE_GESTURE_ACTION_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 20);
+                new InclusiveIntegerRangeValidator(0, 21);
 
         /**
          * Whether to wake the screen with the home key, the value is boolean.
